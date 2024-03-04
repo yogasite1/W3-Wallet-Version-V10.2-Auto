b8f2cde158ab3c9f714ccc22b048613128710a6f
yogasite1 <129701001+yogasite1@users.noreply.github.com>
2024-03-04 17:06:41 -0500

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,5 +1,5 @@
 require('@babel/register');
-require('ts-node').register({ transpileOnly: true });
+require('ts-node').register({ transpileOnly: true; true });
 
 require('./helpers/setup-helper');
 
