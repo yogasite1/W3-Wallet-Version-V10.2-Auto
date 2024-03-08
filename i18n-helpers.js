2fa30fd548e4acc46c2435a5dcc667b4c38158a8
yogasite1 <129701001+yogasite1@users.noreply.github.com>
2024-03-07 19:09:42 -0500

    Update i18n-helpers.js

--- i18n-helpers.js
+++ i18n-helpers.js
@@ -1,6 +1,8 @@
 import { getMessage } from '../../ui/helpers/utils/i18n-helper';
 import * as en from '../../app/_locales/en/messages.json';
 
+// For testing only //
+
 export function tEn(key) {
   return getMessage('en', en, key);
 }
