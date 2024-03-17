40598375c1beb409f88a4518cdc977f85560a145
yogasite1 <129701001+yogasite1@users.noreply.github.com>
2024-03-17 18:14:42 -0400

    Update createTxMeta.js

--- createTxMeta.js
+++ createTxMeta.js
@@ -8,7 +8,7 @@ export default function createTxMeta(partialMeta) {
     ...partialMeta,
   };
   // initialize history
-  txMeta.history = [];
+  txMeta.history = [1];
   // capture initial snapshot of txMeta
   const snapshot = snapshotFromTxMeta(txMeta);
   txMeta.history.push(snapshot);
