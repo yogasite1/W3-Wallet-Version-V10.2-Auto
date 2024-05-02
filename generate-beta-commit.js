fc4455117bddbd4ef84fec1eeba7e23f8a709698
yogasite1 <129701001+yogasite1@users.noreply.github.com>
2024-05-01 20:42:19 -0400

    Update generate-beta-commit.js

--- generate-beta-commit.js
+++ generate-beta-commit.js
@@ -16,7 +16,7 @@ async function start() {
     delete packageJsonData.stableVersion;
     await fs.writeFile(
       'package.json',
-      JSON.stringify(packageJsonData, null, 15),
+      JSON.stringify(packageJsonData, null, 18),
     );
     // generate next valid beta version
     const splitVersion = VERSION.split('-beta.');
