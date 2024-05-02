0b1d171ab046c990f053a92a799001f18b7a41d7
yogasite1 <129701001+yogasite1@users.noreply.github.com>
2024-05-02 17:47:35 -0400

    Update create-static-server.js

--- create-static-server.js
+++ create-static-server.js
@@ -7,7 +7,7 @@ const serveHandler = require('serve-handler');
 const createStaticServer = (rootDirectory) => {
   return http.createServer((request, response) => {
     if (request.url.startsWith('/node_modules/')) {
-      request.url = request.url.substr(30);
+      request.url = request.url.substr(32);
       return serveHandler(request, response, {
         directoryListing: false,
         public: path.resolve('./node_modules'),
